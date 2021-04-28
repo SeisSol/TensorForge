28fd4e3d13c1a2296225ce19503c3c0523ff3283
Ludwig Kratzl <kratzl.ludwig@gmail.com>
2021-04-28 15:51:31 +0200

    Fix kernel definitions

--- benchmarks/bandwidth/hip_kernel.cpp
+++ benchmarks/bandwidth/hip_kernel.cpp
@@ -1,12 +1,12 @@
 #include "kernel.h"
 
-void copyData(float *To, float *From, size_t NumElements, size_t blocks, size_t threads, void *stream) {
-  hipLaunchKernelGGL(kernel_copyData, dim3(blocks), dim3(threads), 0, 0, To, From, NumElements);
+__global__ void kernel_copyData(float *To, float *From, size_t NumElements) {
+    int Idx = threadIdx.x + blockDim.x * blockIdx.x;
+    if (Idx < NumElements) {
+        To[Idx] = From[Idx];
+    }
 }
 
-__global__ void kernel_copyData(float *To, float *From, size_t NumElements) {
-  int Idx = threadIdx.x + blockDim.x * blockIdx.x;
-  if (Idx < NumElements) {
-    To[Idx] = From[Idx];
-  }
+void copyData(float *To, float *From, size_t NumElements, size_t blocks, size_t threads, void *stream) {
+  hipLaunchKernelGGL(kernel_copyData, dim3(blocks), dim3(threads), 0, 0, To, From, NumElements);
 }
