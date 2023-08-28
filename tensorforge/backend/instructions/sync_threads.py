bdd06f1c409961ea21e5d9e67bd8394768bccdb9
David Schneller <david.schneller@tum.de>
2023-08-28 18:52:11 +0200

    Merge sync_threads.py

--- tensorforge/backend/instructions/sync_threads.py
+++ tensorforge/backend/instructions/sync_threads.py
@@ -9,14 +9,15 @@ class SyncThreads(AbstractInstruction):
     self._num_threads = num_threads_per_mult
     self._is_ready = True
 
-  def gen_code(self, writer: Writer):
+  def gen_code(self, writer):
     writer(f'{self.__str__()}')
 
   def __str__(self) -> str:
-    if self._num_threads > self._vm.hw_descr.vec_unit_length:
-      return f'{self._vm.lexic.sync_block_threads};'
+    lexic = self._vm.get_lexic()
+    if self._num_threads > self._vm.get_hw_descr().vec_unit_length:
+      return f'{lexic.sync_threads()};'
     else:
-      return f'{self._vm.lexic.sync_warp_threads};'
+      return f'{lexic.sync_vec_unit()};'
 
   def gen_mask_threads(self, num_threads) -> str:
     return ''
