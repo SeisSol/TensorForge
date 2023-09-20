cd531b766ba84ee9f334947bb60ff91b6a6a8d21
David Schneller <david.schneller@tum.de>
2023-09-20 16:58:24 +0200

    Merge gemms

--- tensorforge/backend/instructions/gemm.py
+++ tensorforge/backend/instructions/gemm.py
@@ -7,7 +7,7 @@ from .abstract_instruction import AbstractInstruction
 from copy import deepcopy
 
 
-class Gemm(AbstractInstruction):
+class ShrMemBasedDenseGemm(AbstractInstruction):
   def __init__(self,
                context: Context,
                trans_a: bool,
@@ -16,7 +16,7 @@ class Gemm(AbstractInstruction):
                op2: Symbol,
                dest: Symbol,
                prefer_align: bool):
-    super(Gemm, self).__init__(context)
+    super(ShrMemBasedDenseGemm, self).__init__(context)
     self._trans_a = trans_a
     self._trans_b = trans_b
     self._op1 = op1
@@ -157,3 +157,110 @@ class Gemm(AbstractInstruction):
 
   def __str__(self):
     return f'{self._dest.name} = gemm {self._op1.name}, {self._op2.name};'
+
+class RegisterOnlyDenseGemm(ShrMemBasedDenseGemm):
+  """This is a gemm operation which utilizes only registers.
+  It performs well on PVC Intel GPUs"""
+
+  def __init__(self,
+               context: Context,
+               trans_a: bool,
+               trans_b: bool,
+               op1: Symbol,
+               op2: Symbol,
+               dest: Symbol,
+               prefer_align: bool):
+    super(RegisterOnlyDenseGemm, self).__init__(context, trans_a, trans_b, op1, op2, dest, prefer_align)
+
+  def gen_code(self, writer):
+    op1_variable = 'value1'
+    op2_variable = 'value2'
+    warp_idx_variable = 'wid'
+
+    op1_data_view = self._op1.data_view
+    op2_data_view = self._op2.data_view
+    thread_idx_x = self._vm.get_lexic().thread_idx_x
+
+    warp_id = self._vm._lexic.get_sub_group_id(self._vec_unit_length)
+    writer(f'auto {warp_idx_variable} = {warp_id};')
+
+    writer(f'{self._vm.fp_as_str()} {op1_variable};')
+    writer(f'{self._vm.fp_as_str()} {op2_variable};')
+
+    active_sub_group_mask = self._vm._lexic.active_sub_group_mask()
+    if active_sub_group_mask:
+      sub_group = 'mask'
+      writer(f'auto {sub_group} = {active_sub_group_mask};')
+    else:
+      sub_group = None
+
+    k_end = op1_data_view.rows if self._trans_a else op1_data_view.columns
+    num_cols = op1_data_view.columns if self._trans_a else op1_data_view.rows
+
+    writer.Emptyline()
+    writer.Pragma('unroll')
+    with writer.For(f'int k = 0; k < {k_end}; ++k'):
+      with writer.If(self.gen_mask_threads(num_cols)):
+        if self._trans_a:
+          op1_addr = f'k + {thread_idx_x} * {op1_data_view.lead_dim}'
+        else:
+          op1_addr = f'{thread_idx_x} + k * {op1_data_view.lead_dim}'
+        writer(f'{op1_variable} = {self._op1.name}[{op1_addr}];')
+
+      start_tile_variable = 'startTileN'
+      end_tile_variable = 'endTileN'
+
+      writer.Emptyline()
+      writer.Pragma('unroll')
+      with writer.For(f'int {start_tile_variable} = 0; '
+                      f'{start_tile_variable} < {self._dest.obj.size}; '
+                      f'{start_tile_variable} += {self._vec_unit_length}'):
+        writer(f'int shiftedWid = {start_tile_variable} + {warp_idx_variable};')
+        with writer.If(f'shiftedWid < {self._dest.obj.size}'):
+          if self._trans_b:
+            op2_addr = f'shiftedWid + k * {op2_data_view.lead_dim}'
+          else:
+            op2_addr = f'shiftedWid * {op2_data_view.lead_dim} + k'
+          writer(f'{op2_variable} = {self._op2.name}[{op2_addr}];')
+
+          if active_sub_group_mask:
+            writer(f'{sub_group} = {active_sub_group_mask};')
+
+        writer.Emptyline()
+        writer(f'int {end_tile_variable} = '
+               f'{start_tile_variable} + {self._vec_unit_length};')
+
+        writer(f'{end_tile_variable} = '
+               f'({end_tile_variable} < {self._dest.obj.size}) '
+               f' ? {end_tile_variable} : {self._dest.obj.size};')
+
+        writer.Emptyline()
+        self._get_inner_loop(writer,
+                             op1_variable,
+                             op2_variable,
+                             start_tile_variable,
+                             end_tile_variable,
+                             sub_group)
+
+  def _get_inner_loop(self,
+                      writer,
+                      op1_variable,
+                      op2_variable,
+                      start,
+                      end,
+                      sub_group):
+    writer.Pragma('unroll')
+    with writer.For(f'int n = {start}, broadcastIdx = 0; '
+                    f'n < {end}; '
+                    f'++n, ++broadcastIdx'):
+      tmp_value = 'tmp'
+      broadcast_sync = self._vm._lexic.broadcast_sync(op2_variable,
+                                                      "broadcastIdx",
+                                                      sub_group)
+      writer(f'auto {tmp_value} = {broadcast_sync};')
+
+      res_access = '' if self._dest.obj.size == 1 else '[n]'
+      writer(f'{self._dest.name}{res_access} += {op1_variable} * {tmp_value};')
+
+  def __str__(self) -> str:
+    return f'{self._dest.name} = rb_gemm({self._op1.name}, {self._op2.name})'
