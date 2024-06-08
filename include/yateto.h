39f7c3a79a4a83b391f563ada30ab0d1ef4225b5
David Schneller <david.schneller@tum.de>
2024-06-09 00:00:45 +0200

    Adjust includes

--- include/yateto.h
+++ include/yateto.h
@@ -1,9 +1,13 @@
 #ifndef YATETO_H_
 #define YATETO_H_
 
+// IWYU pragma: begin_exports
+
 #include "TensorForge/TensorView.h"
 #include "TensorForge/InitTools.h"
 #include "TensorForge/LinearAllocator.h"
 #include "TensorForge/Misc.h"
+
+// IWYU pragma: end_exports
 
 #endif
