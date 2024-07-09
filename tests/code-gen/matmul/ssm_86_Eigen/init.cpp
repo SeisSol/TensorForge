65b2b0aad02c3b00bf8d17921bd5b8ad3fa17471
EliasReutelsterz <elias@reutelsterz.info>
2024-07-09 11:49:23 +0000

    Rebuild CICD pipeline and automate dockerization

---
+++ tests/code-gen/matmul/ssm_86_Eigen/init.cpp
@@ -0,0 +1,9 @@
+#include "init.h"
+namespace yateto {
+  constexpr unsigned const init::A::Start[];
+  constexpr unsigned const init::A::Stop[];
+  constexpr unsigned const init::B::Start[];
+  constexpr unsigned const init::B::Stop[];
+  constexpr unsigned const init::C::Start[];
+  constexpr unsigned const init::C::Stop[];
+} // namespace yateto
