46e88a82c95b8352fd02e6a1bd18cd0fe494027e
David Schneller <david.schneller@tum.de>
2025-07-14 12:14:32 +0200

    Revert "Merge maintainer/setup"
    This reverts commit 98a6245f0880fa39d0bac37a10d74ac2972702a2.

---
+++ setup.py
@@ -0,0 +1,29 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+
+install_requires = ['numpy']
+
+extras = {'with_gpu_support': ['gemmforge==0.0.202']}
+
+setup(
+    name="yateto",
+    version="0.1.0",
+    license="MIT",
+    author="Carsten Uphoff",
+    author_email="uphoff@in.tum.com",
+    description="A tensor toolbox for discontinuous Galerkin methods",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD 3-Clause License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.5',
+    install_requires=install_requires,
+    extras_require=extras,
+)
