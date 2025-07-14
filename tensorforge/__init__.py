b53be67b212ed3133a11a25b1765409796f7c299
David Schneller <david.schneller@tum.de>
2025-07-14 12:13:20 +0200

    Revert "Merge branch 'yateto' into develop"
    This reverts commit 99a237c4627b902c9afffdfb3cc7dd94364f6354, reversing
    changes made to cfb8c0c085d0c5ae51fa9e995cc8c17bdab6a2ea.

--- tensorforge/__init__.py
+++
@@ -1,6 +0,0 @@
-from .type import *
-from .generator import NamespacedGenerator, Generator, simpleParameterSpace, parameterSpaceFromRanges
-from .arch import useArchitectureIdentifiedBy
-from .gemm_configuration import *
-
-from .interop import *
