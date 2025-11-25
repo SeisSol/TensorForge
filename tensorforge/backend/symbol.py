Fix nonlead broadcast

tensorforge/backend/symbol.py
@@ -430,7 +430,7 @@ class Symbol:
         assert len(self.lead_dims) == 1
         idx = index[self.lead_dims[0]]
         if not isinstance(idx, LeadIndex):
-          writevar = lambda var: f'{var}' if isinstance(var, (str, int, float, np.int64)) else var.write(context)
+          writevar = lambda var: f'{var}' if isinstance(var, (str, int, float, np.int64)) else var.write_nonlead()
           access = context.get_vm().get_lexic().broadcast(pre_access, writevar(idx), self.num_threads)
         else:
           access = pre_access