d754db361e84c9c62492fa0f0e6eb21d609e1c4d
David Schneller <david.schneller@tum.de>
2026-02-19 05:14:32 +0100

    Next memmove correction

--- tensorforge/backend/opt/memmove.py
+++ tensorforge/backend/opt/memmove.py
@@ -4,6 +4,7 @@ from tensorforge.backend.instructions.compute import ComputeInstruction
 from tensorforge.backend.instructions.memory import AbstractShrMemWrite, MemoryInstruction
 from tensorforge.backend.instructions.memory.load import LoadInstruction, LoadWait
 from tensorforge.backend.instructions.ptr_manip import GetElementPtr
+from tensorforge.backend.instructions.allocate import RegisterAlloc
 from tensorforge.backend.symbol import SymbolType
 
 class MoveLoads(AbstractTransformer):
@@ -15,19 +16,26 @@ class MoveLoads(AbstractTransformer):
   def apply(self) -> None:
     instrsOut = []
     stored = []
+    def clear_stored(instrsOut):
+        while len(stored) > 0:
+            delayed = stored.pop(0)
+            instrsOut += [delayed]
     for instr in reversed(self._instrs):
         if isinstance(instr, LoadInstruction):
             instrsOut += [LoadWait(instr)]
-            while len(stored) > 0:
-                delayed = stored.pop()
-                instrsOut += [delayed]
+            clear_stored(instrsOut)
             stored.append(instr)
+        elif isinstance(instr, RegisterAlloc):
+            for st in stored:
+                if st._dest is instr._dest:
+                    stored.append(instr)
+                    break
+            else:
+                instrsOut += [instr]
         else:
-            if not isinstance(instr, ComputeInstruction):
-                while len(stored) > 0:
-                    delayed = stored.pop()
-                    instrsOut += [delayed]
+            if isinstance(instr, GetElementPtr):
+                clear_stored(instrsOut)
             instrsOut += [instr]
-    instrsOut += stored[::-1]
+    clear_stored(instrsOut)
 
     self._instrs = instrsOut[::-1]
