b1748195e07accefb87d90ba349c9f33a659ff98
David Schneller <david.schneller@tum.de>
2026-05-18 22:31:58 +0200

    feat: DPP64 for gfx1251

--- tensorforge/backend/instructions/compute/primitives/amd.py
+++ tensorforge/backend/instructions/compute/primitives/amd.py
@@ -359,8 +359,12 @@ def amdarch(ctx):
     archstr = ctx.get_vm().get_hw_descr().model
     return int(archstr[3:], base=16)
 
+def gfx1251(ctx): # no details known yet about the name
+    # gfx1251 supports DPP64 (inferred by the LLVM tests)
+    return amdarch(ctx) == 0x1251
+
 def cdna2(ctx):
-    return amdarch(ctx) < 0x1000 and amdarch(ctx) >= 0x90a
+    return (amdarch(ctx) < 0x1000 and amdarch(ctx) >= 0x90a) or gfx1251(ctx)
 
 def cdna1(ctx):
     return cdna2(ctx) or amdarch(ctx) == 0x908
